35206f13dbd7bdae70b71d78ee7428968b132f34
Денис <deniscikasov@gmail.com>
2025-10-20 20:43:25 +0300

    - текст

--- services/subscriptionService.js
+++ services/subscriptionService.js
@@ -96,7 +96,7 @@ async function activateSubscription(userId, tierId, promoExtendDays = 0) {
       await db.Notification.create({
         user_id: userId,
         title: 'Окончание подписки',
-        message: 'Ваша подписка истекла. Продлите её, чтобы продолжить пользоваться преимуществами.',
+        message: 'Ваш статус истекл. Продлите её, чтобы продолжить пользоваться преимуществами.',
         type: 'warning',
         category: 'subscription',
         importance: 7,
