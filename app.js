8987b2dc3dc0983439b608f2ba31812f052c27d5
Денис <deniscikasov@gmail.com>
2025-10-29 23:37:07 +0300

    фото достижений --

--- app.js
+++ app.js
@@ -67,6 +67,14 @@ app.use('/images', (req, res, next) => {
   next();
 });
 
+app.use('/Achievements', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Cross-Origin-Resource-Policy', 'cross-origin');
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Сессии для Passport
